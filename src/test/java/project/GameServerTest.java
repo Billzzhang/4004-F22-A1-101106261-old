6616b5fdbab0d81dfd1517e412c5aef6bf6f415d
billzzhang <billzhang345@gmail.com>
2022-10-11 16:21:06 -0400

    U-TEST Test48 Test50

--- src/test/java/project/GameServerTest.java
+++ src/test/java/project/GameServerTest.java
@@ -59,4 +59,42 @@ public class GameServerTest {
         int score = gameServer.calculateScore(newRoll);
         assertEquals(score, 0);
     }
+
+    // roll 1 skull, 4 parrots, 3 swords, reroll swords, get 1 skull 2 monkeys
+    //      reroll 2 monkeys, get 1 skull 1 monkey and die
+    @Test
+    void Test48() {
+        FortuneCard fortuneCard = gameServer.drawFortuneCard();
+        fortuneCard = FortuneCard.GOLD;
+        DiceRoll[] rolls = player.rollAllDice();
+        rolls = new DiceRoll[]{DiceRoll.SKULL, DiceRoll.PARROT, DiceRoll.PARROT, DiceRoll.PARROT, DiceRoll.PARROT, DiceRoll.SWORD, DiceRoll.SWORD, DiceRoll.SWORD};
+        DiceRoll[] newRoll = player.reroll(rolls, new int[]{5, 6, 7});
+        newRoll[5] = DiceRoll.SKULL;
+        newRoll[6] = DiceRoll.MONKEY;
+        newRoll[7] = DiceRoll.MONKEY;
+        newRoll = player.reroll(newRoll, new int[]{6, 7});
+        newRoll[6] = DiceRoll.SKULL;
+        newRoll[7] = DiceRoll.MONKEY;
+        int score = gameServer.calculateScore(newRoll);
+        assertEquals(score, 0);
+    }
+
+    //roll 1 skull, 2 parrots, 3 swords, 2 coins, reroll parrots get 2 coins
+    //      reroll 3 swords, get 3 coins (SC 4000 for seq of 8 (with FC coin) + 8x100=800 = 4800)
+    @Test
+    void Test50(){
+        FortuneCard fortuneCard = gameServer.drawFortuneCard();
+        fortuneCard = FortuneCard.GOLD;
+        DiceRoll[] rolls = player.rollAllDice();
+        rolls = new DiceRoll[]{DiceRoll.SKULL, DiceRoll.PARROT, DiceRoll.PARROT, DiceRoll.GOLD, DiceRoll.GOLD, DiceRoll.SWORD, DiceRoll.SWORD, DiceRoll.SWORD};
+        DiceRoll[] newRoll = player.reroll(rolls, new int[]{1, 2});
+        newRoll[1] = DiceRoll.GOLD;
+        newRoll[2] = DiceRoll.GOLD;
+        newRoll = player.reroll(newRoll, new int[]{5, 6, 7});
+        newRoll[5] = DiceRoll.GOLD;
+        newRoll[6] = DiceRoll.GOLD;
+        newRoll[7] = DiceRoll.GOLD;
+        int score = gameServer.calculateScore(newRoll);
+        assertEquals(score, 4800);
+    }
 }
